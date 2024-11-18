fd5d8fc77396d8cd55179459a19b537be9375710
Sarhane Ghribi <sarhane.ghribi@infinitum-tech.tn>
2024-11-18 15:36:16 +0100

    starting phone

---
+++ src/app/components/phone/PhoneLoadingScreen.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+const PhoneLoadingScreen: React.FC = () => {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <div className="w-16 h-16 border-t-4 border-blue-500 border-solid rounded-full animate-spin"></div>
+      <p className="mt-4 text-gray-600">Loading...</p>
+    </div>
+  );
+};
+export default PhoneLoadingScreen;
